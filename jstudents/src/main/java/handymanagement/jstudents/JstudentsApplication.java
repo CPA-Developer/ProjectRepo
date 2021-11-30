b4b3e6ccaac894dff7f49f5ca3ebd8bb57fc9c10
CPA-Developer <subinsebastian017@gmail.com>
2021-11-30 11:16:10 -0500

    create javaProject

---
+++ jstudents/src/main/java/handymanagement/jstudents/JstudentsApplication.java
@@ -0,0 +1,13 @@
+package handymanagement.jstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstudentsApplication.class, args);
+	}
+
+}
---
+++ jstudents/src/test/java/handymanagement/jstudents/JstudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package handymanagement.jstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
